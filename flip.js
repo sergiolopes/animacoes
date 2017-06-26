8217537a7b72c8c07ef36b9528717ca733f82eb0
Sérgio Lopes <sergio.lopes@caelum.com.br>
2017-06-26 17:28:58 -0300

    extraindo

---
+++ flip.js
@@ -0,0 +1,41 @@
+var lista = document.querySelector('.lista');
+var cartoes = Array.prototype.slice.call(document.querySelectorAll('.cartao'));
+
+cartoes.forEach((cartao) => cartao.querySelector('button').addEventListener('click', cartaoOnClick));
+
+function cartaoOnClick() {
+    var cartao = this.parentElement;
+    preparaAnimacao(cartao);
+    requestAnimationFrame(disparaAnimacao);
+    cartao.addEventListener('transitionend', aposAnimacao);
+}
+
+function preparaAnimacao(cartaoARemover) {
+    // (opcional) corner case pra multiplas colunas
+    var posCartaoARemover = cartaoARemover.getBoundingClientRect();
+    var posLista = lista.getBoundingClientRect();
+    cartaoARemover.style.top = posCartaoARemover.top - posLista.top - 10 + 'px';
+    cartaoARemover.style.left = posCartaoARemover.left - posLista.left - 10 + 'px';
+
+    // pega posicoes dos elementos antes e depois da modificação
+    var posInicial = cartoes.map((cartao) => cartao.getBoundingClientRect());
+    cartaoARemover.classList.add('remove');
+
+    // aplica um transform pra colocar cada cartao de volta no lugar dele
+    cartoes.forEach((cartao, i) => {
+        var posFinal = cartao.getBoundingClientRect();
+        var x = posInicial[i].left - posFinal.left;
+        var y = posInicial[i].top - posFinal.top;
+        cartao.style.transform = `translate(${x}px, ${y}px)`;
+    });
+}
+
+function disparaAnimacao() {
+    cartoes.forEach((cartao) => cartao.style.transform = '' );
+    lista.classList.add('anima');
+}
+
+function aposAnimacao() {
+    lista.classList.remove('anima');
+    this.remove();
+}
